500b2421ec2daf2f02fb0d13b4246b42519b209a
Yushi <yushari@seznam.cz>
2026-02-05 14:25:23 +0100

    added navbar class to render the navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,1 +1,44 @@
-import 'Navbar.css';
+import './Navbar.css';
+import hamburgerIcon from '../../assets/images/icons/hamburger-menu.svg';
+
+export class Navbar {
+    constructor() {
+        this.container = document.getElementById('header');
+        this.activeLink = null;
+    }
+
+    render() {
+        this.container.innerHTML = `
+            <nav class="navbar">
+                <ul class="nav__list">
+                    <li class="nav__item"><a href="#home" class="nav-link active">Home</a></li>
+                    <li class="nav__item"><a href="#menu" class="nav-link">Menu</a></li>
+                    <li class="nav__item"><a href="#about" class="nav-link">About</a></li>
+                    <li class="nav__item"><a href="#contact" class="nav-link">Contact</a></li>
+                </ul>
+                <button class="menu__toggle" aria-label="Toggle menu" type="button">
+                    <img 
+                    src="${hamburgerIcon}" 
+                    alt="" 
+                    class="menu__icon"
+                    width="40"
+                    height="40"
+                    />
+                </button>
+            </nav>
+        `;
+    }
+
+    AddEventListeners() {
+        const links = this.container.querySelectorAll('.nav__link');
+        links.forEach((link) => {
+            link.addEventListener('click', () => {
+                if (this.activeLink) {
+                    this.activeLink.classList.remove('active');
+                }
+                link.classList.add('active');
+                this.activeLink = link;
+            });
+        });
+    }
+}
