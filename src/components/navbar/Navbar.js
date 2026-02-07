0c781ec9d5e7d9f60370c256e24ee13e65e05615
Yushi <yushari@seznam.cz>
2026-02-07 13:43:24 +0100

    added title to nav

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -41,6 +41,7 @@ export class Navbar {
                     />
 
                 <ul class="nav__list">
+                    <h1 class="nav__title">Welcome!</h1>
                     <li class="nav__item"><a href="#home" class="nav__link active">Home</a></li>
                     <li class="nav__item"><a href="#menu" class="nav__link">Menu</a></li>
                     <li class="nav__item"><a href="#about" class="nav__link">About</a></li>
