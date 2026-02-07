be80c48aad02b28317b6bb516b92d05cd27cb955
Yushi <yushari@seznam.cz>
2026-02-07 15:35:03 +0100

    added html markup for the carousel

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,3 +1,8 @@
+// Images Imports
+import OpeningIntro from '../../assets/images/opening-intro.png';
+import premiumSteakIntro from '../../assets/images/premium-steak-intro.png';
+import steakSaleIntro from '../../assets/images/steak-sale-intro.png';
+
 export class HomePage {
   constructor() {
     this.container = document.getElementById('main');
@@ -7,9 +12,21 @@ export class HomePage {
     render() {
         this.container.innerHTML = 
         `<div class="home__page">
-            <h1>Welcome to our restaurant!</h1>
-            <p>We serve delicious food made with fresh ingredients. Come and enjoy a meal with us!</p>
-        </div>`
-        ;
+
+            <div class="home__page--intro">
+              <img src="${OpeningIntro}" alt="Opening Intro" class="home__page--intro-image">
+
+                <div class="carousel">
+                  <div class="carousel-item active">
+                    <img src="${premiumSteakIntro}" alt="Premium Steak Intro" class="carousel-image">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="${steakSaleIntro}" alt="Steak Sale Intro" class="carousel-image">
+                  </div>
+
+                </div>
+            </div>
+
+        </div>`;
     }
 }
