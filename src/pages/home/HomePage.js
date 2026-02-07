50343d65ba8e3ad4bfe1e67d86b3a3b7d72f551d
Yushi <yushari@seznam.cz>
2026-02-07 15:36:38 +0100

    imported homepage css file

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -3,6 +3,8 @@ import OpeningIntro from '../../assets/images/opening-intro.png';
 import premiumSteakIntro from '../../assets/images/premium-steak-intro.png';
 import steakSaleIntro from '../../assets/images/steak-sale-intro.png';
 
+import './HomePage.css';
+
 export class HomePage {
   constructor() {
     this.container = document.getElementById('main');
@@ -23,8 +25,8 @@ export class HomePage {
                   <div class="carousel-item">
                     <img src="${steakSaleIntro}" alt="Steak Sale Intro" class="carousel-image">
                   </div>
+                </div>    
 
-                </div>
             </div>
 
         </div>`;
