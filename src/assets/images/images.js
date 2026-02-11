061a363e08226da9ee69ed92cfd841832aa3e489
Yushi <yushari@seznam.cz>
2026-02-11 17:17:38 +0100

    imported images into a single file

---
+++ src/assets/images/images.js
@@ -0,0 +1,27 @@
+import OpeningIntro from '../../assets/images/opening-intro.png';
+import premiumSteakIntro from '../../assets/images/premium-steak-intro.png';
+import steakSaleIntro from '../../assets/images/steak-sale-intro.png';
+import reservationBg from '../../assets/images/reservation-bg.png';
+import freshBeef from '../../assets/images/fresh-beef.png';
+import localStories from '../../assets/images/local-stories.png';
+import insta01 from '../../assets/images/insta_01.png';
+import insta02 from '../../assets/images/insta_02.png';
+import insta03 from '../../assets/images/insta_03.png';
+import insta04 from '../../assets/images/insta_04.png';
+import insta05 from '../../assets/images/insta_05.png';
+import insta06 from '../../assets/images/insta_06.png';
+
+export const FoodImages = {
+    OpeningIntro,
+    premiumSteakIntro,
+    steakSaleIntro,
+    reservationBg,
+    freshBeef,
+    localStories,
+    insta01,
+    insta02,
+    insta03,
+    insta04,
+    insta05,
+    insta06
+};
