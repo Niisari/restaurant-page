855c5dcb4cc8854253e514b2e772597c698b658e
Yushi <yushari@seznam.cz>
2026-02-11 17:47:45 +0100

    rendered footer

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import "./styles/Reset.css";
 
 import { Navbar } from "./components/navbar/Navbar.js";
 import { HomePage } from "./pages/home/HomePage.js";
+import { Footer } from "./components/footer/Footer.js";
 
 const navbar = new Navbar('header');
 navbar.render();
 
 const homePage = new HomePage();
 homePage.render();
+
+const footer = new Footer();
+footer.render();
