cbe586ecd0f157a1ac3a980487ecc02ef942b1e3
Yushi <yushari@seznam.cz>
2026-02-12 15:23:49 +0100

    finished tiny header above the menu

--- src/pages/menu/MenuPage.js
+++ src/pages/menu/MenuPage.js
@@ -1,7 +1,21 @@
 import './MenuPage.css';
+import { button } from '../../components/buttons/button.js';
 export class MenuPage {
     constructor() {
         this.container = document.getElementById('main');
         this.link = '/menu';
+    }
+
+    render() {
+        this.container.innerHTML = `
+            <div class="menu__container">
+                <div class="menu__header">
+                    <h2>Bone-IN Ribeye</h2>
+                    <p>20oz. cut of our juicy, flavorful ribeye served on the bone for extra flavor.</p>
+                    <a>${button('Order Now')}</a>
+                </div>
+                <h1>Menu</h1>
+            </div>
+        `;
     }
 }
