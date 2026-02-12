e273bd4310d2a8ded0ae7a900baff20137e600db
Yushi <yushari@seznam.cz>
2026-02-12 15:46:11 +0100

    created the carousel x menu for the food categories

--- src/pages/menu/MenuPage.js
+++ src/pages/menu/MenuPage.js
@@ -14,7 +14,64 @@ export class MenuPage {
                     <p>20oz. cut of our juicy, flavorful ribeye served on the bone for extra flavor.</p>
                     <a>${button('Order Now')}</a>
                 </div>
-                <h1>Menu</h1>
+                
+                <div class="menu__content">
+                    <div class="menu__categories">
+                        <ul class="menu__categories--list">
+                            <li>
+                                <a>JUST FOR STARTERS</a>
+                            </li>
+                            <li>
+                                <a>HAND-CUT STEAKS</a>
+                            </li>
+                            <li>
+                                <a>THE BONE RIBS</a>
+                            </li>
+                            <li>
+                                <a>ALABAMA SIZE COMBOS</a>
+                            </li>
+                            <li>
+                                <a>DOCKSIDE FAVORITES</a>
+                            </li>
+                            <li>
+                                <a>CHICKEN SPECIALTIES</a>
+                            </li>
+                            <li>
+                                <a>COUNTRY DINNERS</a>
+                            </li>
+                            <li>
+                                <a>SALADS</a>
+                            </li>
+                            <li>
+                                <a>BURGERS & SANDWICHES</a>
+                            </li>
+                            <li>
+                                <a>SIDES & EXTRAS</a>
+                            </li>
+                            <li>
+                                <a>KIDS & RANGER MEALS</a>
+                            </li>
+                            <li>
+                                <a>BEVERAGES</a>
+                            </li>
+                            <li>
+                                <a>DESSERTS</a>
+                            </li>
+                            <li>
+                                <a>MARGARITAS</a>
+                            </li>
+                            <li>
+                                <a>COCKTAILS</a>
+                            </li>
+                            <li>
+                                <a>BEER</a>
+                            </li>
+                            <li>
+                                <a>WINE</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         `;
     }
