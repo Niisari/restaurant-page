44ceaa7bf8d36f6d6a67fe735da5cd4424a0e9a5
Yushi <yushari@seznam.cz>
2026-02-14 17:18:46 +0100

    Imported AboutPage and added it to router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // App.js
 import { Navbar } from "./components/navbar/Navbar.js";
 import { HomePage } from "./pages/home/HomePage.js";
-import { MenuPage } from "./pages/menu/MenuPage.js"; // Create this!
+import { MenuPage } from "./pages/menu/MenuPage.js"; 
+import { AboutPage } from "./pages/about/About.js";
 import { Footer } from "./components/footer/Footer.js";
 
 export default class App {
@@ -13,7 +14,7 @@ export default class App {
         this.routes = {
             '/': new HomePage(),
             '/menu': new MenuPage(),
-            // '/about': new AboutPage(),
+            '/about': new AboutPage(),
         };
     }
 
