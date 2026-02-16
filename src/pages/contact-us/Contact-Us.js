3da225c623dd99aa27ed62acf7ff74405289578c
Yushi <yushari@seznam.cz>
2026-02-16 15:00:51 +0100

    finished contact form

--- src/pages/contact-us/Contact-Us.js
+++ src/pages/contact-us/Contact-Us.js
@@ -83,7 +83,7 @@ export class ContactUsPage {
                                 </button>
 
                                 <div class="faq__answer">
-                                    <p><a>Check your balance here</a></p>
+                                    <p>Currently, we are experiencing long delays in order processing and shipping.  Additionally, gift cards ordered with USPS shipping do not have a tracking number.  To ensure that your gift card reaches the recipient in a timely manner, please consider sending an electronic gift card.</p>
                                 </div>
                             </div>
 
@@ -94,7 +94,10 @@ export class ContactUsPage {
                                 </button>
 
                                 <div class="faq__answer">
-                                    <p><a>Check your balance here</a></p>
+                                    <p>Your gift card was sent from 
+                                    <a>varxysteakhousegiftcards@cashstar.com</a>
+                                    You might need to add this email address to your list of trusted senders or check your spam folder.
+                                    </p>
                                 </div>
                             </div>
 
@@ -105,7 +108,10 @@ export class ContactUsPage {
                                 </button>
 
                                 <div class="faq__answer">
-                                    <p><a>Check your balance here</a></p>
+                                    <p>
+                                    Our gift card specials normally run during the holidays. Keep an eye on our website and touch base every now and then with your favorite location as they might offer specials specific to their location. 
+                                    <a>https://www.varxysteakhouse.com/gift-cards</a>
+                                    </p>
                                 </div>
                             </div>
 
@@ -116,7 +122,10 @@ export class ContactUsPage {
                                 </button>
 
                                 <div class="faq__answer">
-                                    <p><a>Check your balance here</a></p>
+                                    <p>
+                                    Here is the link to purchase our electronic or physical gift cards, 
+                                    <a>https://www.varxysteakhouse.com/gift-cards</a>
+                                    </p>
                                 </div>
                             </div>
                             
@@ -138,7 +147,9 @@ export class ContactUsPage {
                                 </button>
 
                                 <div class="faq__answer">
-                                    <p><a>Check your balance here</a></p>
+                                    <p><a>Sign up for our VIP Club</a>
+                                     you will receive a free appetizer within the first 24-48hrs. Then you will receive goodies every year on your birthday, anniversary of when you sign up and if your location decides to send anything else out. You can also text VIP to 68984.
+                                    </p>
                                 </div>
                             </div>
 
@@ -149,7 +160,10 @@ export class ContactUsPage {
                                 </button>
 
                                 <div class="faq__answer">
-                                    <p><a>Check your balance here</a></p>
+                                    <p>
+                                    Our signature seasoning and sauce is now on Amazon! Just click this link: 
+                                    <a>https://www.amazon.com/s?k=texas+roadhouse&ref=nb_sb_noss_2</a>
+                                    </p>
                                 </div>
                             </div>
 
@@ -160,7 +174,7 @@ export class ContactUsPage {
                                 </button>
 
                                 <div class="faq__answer">
-                                    <p><a>Check your balance here</a></p>
+                                    <p>As much as we would love to share our recipes with you, all of our recipes are proprietary. But you can still absolutely call your local Texas Roadhouse to see if they can provide you with your favorite menu item to use at home for your dinner!  </p>
                                 </div>
                             </div>
                             
@@ -171,7 +185,9 @@ export class ContactUsPage {
                                 </button>
 
                                 <div class="faq__answer">
-                                    <p><a>Check your balance here</a></p>
+                                    <p>
+                                    <a href="/menu">view our menu.</a>
+                                    </p>
                                 </div>
                             </div>                             
 
@@ -182,7 +198,11 @@ export class ContactUsPage {
                                 </button>
 
                                 <div class="faq__answer">
-                                    <p><a>Check your balance here</a></p>
+                                    <p>
+                                    We do not have a reward program with our mobile app, but if you go to 
+                                    <a>https://www.varxysteakhouse.com/vip-club</a>
+                                     and sign up for our VIP Club you will receive a free appetizer within the first 24-48hrs. Additionally, you will receive goodies every year on your birthday and anniversary of sign-up date.
+                                    </p>
                                 </div>
                             </div>
 
@@ -193,7 +213,7 @@ export class ContactUsPage {
                                 </button>
 
                                 <div class="faq__answer">
-                                    <p><a>Check your balance here</a></p>
+                                    <p>It’s possible that you have replied “Stop” to our messages in the past and thus have stopped receiving them. You can reply to our company number: 855-540-1372 with the word “Start” and that will allow the text messages to come through again.</p>
                                 </div>
                             </div>
 
@@ -204,7 +224,10 @@ export class ContactUsPage {
                                 </button>
 
                                 <div class="faq__answer">
-                                    <p><a>Check your balance here</a></p>
+                                    <p>
+                                    We are testing and researching alternative options at this very moment. We just moved to paper bags to pack our fresh-baked bread as well as some locations using paper cups instead of Styrofoam! We too want to do what is best for our guests and the environment. Please 
+                                    <a>Learn more about our sustainability efforts.</a>
+                                    </p>
                                 </div>
                             </div>
                             
@@ -215,9 +238,41 @@ export class ContactUsPage {
                                 </button>
 
                                 <div class="faq__answer">
-                                    <p><a>Check your balance here</a></p>
+                                    <p>The artist for our VARXY STEAKHOUSE theme song is Kendrick Lamar.</p>
                                 </div>
-                            </div>                         
+                            </div>
+                            
+                            <form action="https://formspree.io/f/xqkqyjyj" method="POST">
+                                <div class="form__container">
+                                    <h2 class="form__title">CONTACT US</h2>
+
+                                        <label for="firstName" class="label">First Name: *</label>
+                                        <input type="text" name="firstName" placeholder="Barrack" required>
+
+                                        <label for="lastName" class="label">Last Name: *</label>
+                                        <input type="text" name="lastName" placeholder="Obama" required>
+
+                                        <label for="phone" class="label">Phone Number: *</label>
+                                        <input type="number" name="phone" placeholder="640-123-4567" required>
+
+                                        <label for="email" class="label">Email: *</label>
+                                        <input type="email" name="email" placeholder="BarrackObama@gmail.com" required>
+
+                                        <label for="confirmEmail" class="label">Confirm Email: *</label>
+                                        <input type="email" name"confirmEmail" placeholder="Confirm Email" required>
+
+                                        <label for="subject" class="label">What's Going on? *</label>
+                                        <select name="subject" id="subject" required>
+                                            <option value="Recent Order">Recent Order</option>
+                                            <option value="General Inquiry">General Inquiry</option>
+                                            <option value="Gift Cards">Gift Cards</option>
+                                            <option value="Manage my account">Manage my account</option>
+                                        </select>
+                                    <div class="form__button--container">
+                                        <button type="submit" value="Submit" class="form__button">${button("Send Message")}</button>
+                                    </div>
+                                </div>               
+                            </form>
 
                     </section>
 
