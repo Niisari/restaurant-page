4bb31157efc225dff324c983f2c70043ce8ef785
Yushi <yushari@seznam.cz>
2026-02-16 15:53:27 +0100

    added background image for the about us intro

--- src/pages/about/About-us.js
+++ src/pages/about/About-us.js
@@ -1,9 +1,20 @@
+import "./About-us.css";
 export class AboutPage {
     constructor() {
         this.container = document.getElementById('main');
         this.link = '/about-us';
     }
     render() {
-        this.container.innerHTML = `<h1>About Page</h1>`
+        this.container.innerHTML = `
+        <section class="about__page">
+            <div class="about__content">
+
+                <div class="about__image--bg">
+                </div>
+
+            </div>
+
+        </section>
+        `
     }
 }
