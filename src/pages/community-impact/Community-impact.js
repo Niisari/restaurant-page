8f0b067d27ddd7cb94d1dd71830c8222fde75824
Yushi <yushari@seznam.cz>
2026-02-20 15:01:39 +0100

    finished desktop design

--- src/pages/community-impact/Community-impact.js
+++ src/pages/community-impact/Community-impact.js
@@ -25,19 +25,27 @@ export class CommunityImpactPage {
 
                     <div class="community__content">
                         <div class="community__content--item">
-                            <img src="${FoodImages.CommunityPhoto01}"/>
-                            <h3>WE HONOR HEROES</h3>
-                            <p>We’ve been a national partner with Homes For Our Troops (HFOT) for more than 17 years, 
-                            completing over 300 mortgage-free, custom-built homes for the most severely injured post-9/11 veterans. And on Veterans Day, 
-                            we're honored to to offer free meals to veterans and active military members across the country.</p>
+                            <div class="community__content--image">
+                                <img src="${FoodImages.CommunityPhoto01}"/>
+                            </div>
+                            <div class="community__content--text">
+                                <h3>WE HONOR HEROES</h3>
+                                <p>We’ve been a national partner with Homes For Our Troops (HFOT) for more than 17 years, 
+                                completing over 300 mortgage-free, custom-built homes for the most severely injured post-9/11 veterans. And on Veterans Day, 
+                                we're honored to to offer free meals to veterans and active military members across the country.</p>
+                            </div>
                         </div>
 
-                        <div class="community__content--item">
-                            <img src="${FoodImages.CommunityPhoto02}"/>
-                            <h3>PLANTING IT FORWARD</h3>
-                            <p>
-                            As part of our Corporate Sustainability program, we have been proud to partner with the Arbor Day Foundation since 2017. Each year, we donate $50,000 to support the Arbor Day Foundation's Community Tree Recovery Campaign and host tree distribution events in communities across the country.
-                            </p>
+                        <div class="community__content--reverse">
+                            <div class="community__content--image">
+                                <img src="${FoodImages.CommunityPhoto02}"/>
+                            </div>
+                            <div class="community__content--text">
+                                <h3>PLANTING IT FORWARD</h3>
+                                <p>
+                                As part of our Corporate Sustainability program, we have been proud to partner with the Arbor Day Foundation since 2017. Each year, we donate $50,000 to support the Arbor Day Foundation's Community Tree Recovery Campaign and host tree distribution events in communities across the country.
+                                </p>
+                            </div>
                         </div>
                     </div>
 
