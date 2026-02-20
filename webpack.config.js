2641e73017a91e658589f0971a88c4285f7ae60a
Yushi <yushari@seznam.cz>
2026-02-20 19:13:44 +0100

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,40 @@
 import path from "node:path";
+import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin"; 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
+    path: path.resolve(__dirname, "docs"), // Use __dirname we defined above
     clean: true,
-    publicPath: '',
+    // Ensure this matches your GitHub repository name exactly
+    publicPath: '/restaurant-page/', 
   },
-    devtool: "eval-source-map",
-    devServer: {
+  devtool: "eval-source-map",
+  devServer: {
     historyApiFallback: true,
     hot: true,
     watchFiles: ["./src/template.html"],
+    // Added static setting so dev server can see your public folder locally
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        // This copies public/images into docs/images
+        { from: "public", to: "" }, 
+      ],
     }),
   ],
   module: {
